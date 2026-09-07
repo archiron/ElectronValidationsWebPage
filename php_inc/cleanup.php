1172c068697f2dc2c63e80ede5d32e73e24f4e2c
archiron <arnaud.chiron@cern.ch>
2026-09-07 14:00:56 +0200

    Version 2026.09.07-03
    more cleaning

--- php_inc/cleanup.php
+++ php_inc/cleanup.php
@@ -13,22 +13,10 @@ if (isset($_POST['file_to_delete'])) {
     
     // CONSTRUCTION DU CHEMIN : Ajoutez 'BasketList/' si vos fichiers y sont stockés
     // Si vos fichiers sont à la racine de $baseDir, retirez '/BasketList'
-    $path = $baseDir . 'BasketList/' . $file; 
-
-    // --- DEBUG ---
-    // On renvoie le chemin testé dans la réponse (même en HTML)
-    echo "<strong>DEBUG INFO:</strong><br>";
-    echo "Chemin testé : " . $path . "<br>";
-    echo "Fichier existe : " . (file_exists($path) ? 'OUI' : 'NON') . "<br>";
-    echo "Nom reçu : " . $file . "<br>";
-    if (is_dir($baseDir . 'BasketList/')) {
-        echo "Le dossier BasketList existe et est lisible.";
-    } else {
-        echo "Le dossier BasketList n'existe pas ou n'est pas lisible.";
-    }
-    // ---------------
+    $path = basename($baseDir . 'BasketList/' . $file); 
 
     if (file_exists($path)) {
+        error_log("DELETE: {$_SERVER['REMOTE_USER']} -> {$file}");
         if (unlink($path)) {
             http_response_code(200);
             echo "SUCCES: Fichier supprimé.";
